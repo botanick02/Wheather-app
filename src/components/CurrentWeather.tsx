673d971703e351ea839998770da1dbc35abb0208
Danyil Tuhaienko <botanick02@gmail.com>
2023-11-08 21:39:16 +0200

    added react imports

--- src/components/CurrentWeather.tsx
+++ src/components/CurrentWeather.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 const CurrentWeather = () => {
   return (
     <div className="current-weather">
--- src/components/WeatherDaily.tsx
+++ src/components/WeatherDaily.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import DailyWeatherItem from "../elements/DailyWeatherItem";
 
 const WeatherDaily = () => {
