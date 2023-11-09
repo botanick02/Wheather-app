6085348d186d331607d896f0ad0862db38624ec4
Danyil Tuhaienko <botanick02@gmail.com>
2023-11-10 01:30:58 +0200

    figma template realization

--- src/components/CurrentWeather.tsx
+++ src/components/CurrentWeather.tsx
@@ -3,20 +3,19 @@ import React from "react";
 const CurrentWeather = () => {
   return (
     <div className="current-weather">
-      <div className="current-weather__location">Kyiv, Ukraine</div>
+      <div className="current-weather__day">Monday</div>
+      <div className="current-weather__location">Sacramento</div>
       <div className="current-weather__main">
-        <img
-          src={require("../img/sun.png")}
-          alt="weather img"
-          className="current-weather__main__visual"
-        />
-        <div className="current-weather__main__temp">+10°C</div>
+        <div className="current-weather__main__temp">+17°C</div>
+        <div className="current-weather__main__desc">Sunny</div>
       </div>
-      <div className="current-weather__details">
-        <div className="current-weather__details__description">
-          Mostly clear
+      <div className="current-weather__maxmin">
+        <div className="current-weather__details__maxmin__max">
+          Max.<div>+17°C</div>
         </div>
-        <div className="current-weather__details__maxmin">+12°C +6°C</div>
+        <div className="current-weather__details__maxmin__min">
+          Min.<div>+15°C</div>
+        </div>
       </div>
     </div>
   );
--- src/elements/DailyWeatherItem.tsx
+++ src/elements/DailyWeatherItem.tsx
@@ -3,13 +3,44 @@ import React from "react";
 const DailyWeatherItem = () => {
   return (
     <div className="daily-item">
-      <div className="daily-item__date">
-        25 November
-        <br />
-        Friday
+      <div className="daily-item__day">Friday</div>
+      <div className="daily-item__card">
+        <div className="daily-item__card__date">01.01</div>
+        <img
+          src={require("../img/sunny.png")}
+          alt=""
+          className="daily-item__card__visual"
+        />
+        <div className="daily-item__card__descr">Sunny</div>
+        <div className="daily-item__card__temp">+16°C</div>
+        <div className="daily-item__card__rain">Chance of rain: 15%</div>
+        <div className="daily-item__card__maxmin">
+          <div>
+            Max.<div>+17°C</div>
+          </div>
+          <div>
+            Min.<div>+15°C</div>
+          </div>
+        </div>
+        <div className="daily-item__card__suntime">
+          <div>
+            <img
+              src={require("../img/sunrise.png")}
+              alt=""
+              className="daily-item__card__visual"
+            />
+            <div>6:00AM</div>
+          </div>
+          <div>
+            <img
+              src={require("../img/sunset.png")}
+              alt=""
+              className="daily-item__card__visual"
+            />
+            <div>6:00PM</div>
+          </div>
+        </div>
       </div>
-      <img src={require("../img/sun.png")} alt="" className="daily-item__visual" />
-      <div className="daily-item__temp">+10°C +6°C</div>
     </div>
   );
 };
