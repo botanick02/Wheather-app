89c26166c3af4fabf69739119b53e16bb21bc82c
Danyil Tuhaienko <botanick02@gmail.com>
2023-11-09 10:29:59 +0200

    feat: serach bar input placeholder

--- src/elements/SearchBar.tsx
+++ src/elements/SearchBar.tsx
@@ -4,7 +4,7 @@ import { ReactComponent as SearchIcon } from "../img/svg/search.svg";
 const SearchBar = () => {
   return (
     <div className="search-bar">
-      <input type="text" className="search-bar__input" />
+      <input type="text" placeholder="Search..." className="search-bar__input" />
       <div className="search-bar__submit">
         <SearchIcon/>
       </div>
